1b6356ef385060eb9c7773a8a07d48f791910b99
cmis <cmis@cmis-hs08.(none)>
2016-03-02 09:55:17 +0700

    Quizz

---
+++ cs2quiz.py
@@ -0,0 +1,51 @@
+#Part 1: Terminology (15 points)
+#1 1pt) What is the symbol "=" used for?
+# It is used as an assignment opperator. 
+# It is used to put value in a variable.
+# Converts variable into a value.
+#2 3pts) Write a technical definition for 'function'
+# Statements that are named and perform a computation. 
+# 
+#
+#3 1pt) What does the keyword "return" do?
+# It spits out an output. 
+# Returns value from a function. 
+#
+#4 5pts) We know 5 basic data types. Write the name for each one and provide two
+#   examples of each below
+#	1: boolean True or False
+#	2: interger ex.2,3
+#	3: floating ex. 0.2,4.5
+#	4: string "I like Cats" "36"
+#	5: tuple ("Vanda", 16 )
+#
+#5 2pts) What is the difference between a "function definition" and a 
+#        "function call"?
+#Function definition is when you define a fuction.
+#Function call is when you call the fuction by its' name.
+#
+#
+#6 3pts) What are the 3 phases that every computer program has? What happens in
+#        each of them
+#	1: Input
+#	2: calling function
+#	3: Output
+#
+
+C1= raw_input ("Area of C1:")
+C2= raw_input ("Area of C2:")
+C3= raw_input ("Area of C3:")
+Totals= int(c1) + int(c2) + int(c3)
+
+Lists= ["Circle", "Diameter"]
+import math
+
+    
+
+
+
+
+
+
+
+
