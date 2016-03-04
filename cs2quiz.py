9059ae785d34408aba91b06887b41bcf22d01c10
cmis <cmis@cmis-hs08.(none)>
2016-03-04 13:48:39 +0700

    graded

--- cs2quiz.py
+++ cs2quiz.py
@@ -1,16 +1,20 @@
 #Part 1: Terminology (15 points)
++1 defined "="
 #1 1pt) What is the symbol "=" used for?
 # It is used as an assignment opperator. 
 # It is used to put value in a variable.
 # Converts variable into a value.
++3 defined function
 #2 3pts) Write a technical definition for 'function'
 # Statements that are named and perform a computation. 
 # 
 #
++ 1 defined return
 #3 1pt) What does the keyword "return" do?
 # It spits out an output. 
 # Returns value from a function. 
 #
++4.5 for not doing another tuple
 #4 5pts) We know 5 basic data types. Write the name for each one and provide two
 #   examples of each below
 #	1: boolean True or False
@@ -19,18 +23,25 @@
 #	4: string "I like Cats" "36"
 #	5: tuple ("Vanda", 16 )
 #
++1 because not fully described
 #5 2pts) What is the difference between a "function definition" and a 
 #        "function call"?
 #Function definition is when you define a fuction.
 #Function call is when you call the fuction by its' name.
 #
 #
++1.5 for not explaining the 3 phases 
 #6 3pts) What are the 3 phases that every computer program has? What happens in
 #        each of them
 #	1: Input
 #	2: process
 #	3: Output
 #
++1 getting input
++1 calling output
++1 converted input
++1 variable names
+
 
 C1= raw_input ("Area of C1:")
 C2= raw_input ("Area of C2:")
