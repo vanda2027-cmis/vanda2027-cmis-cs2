3aff4f6d3d4560102dc1c0920e7f2a029a34c7c5
cmis <cmis@cmis-hs08.(none)>
2016-03-10 14:35:34 +0700

    made changes

---
+++ oneguess.py
@@ -0,0 +1,7 @@
+def ():
+	return 
+
+def main ():
+minimum = raw_input ("What is the minimum number?: ")
+maximum = raw_input ("What is the maximum number?: ")
+thinking = raw_input ("I'm thinking of a number:")
