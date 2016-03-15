e4eb154dbded857de6468372292cb994d11dfe34
cmis <cmis@cmis-hs08.(none)>
2016-03-15 09:57:19 +0700

    madechanges

--- oneguess.py
+++ oneguess.py
@@ -1,7 +1,28 @@
-def ():
-	return 
+
 
 def main ():
-minimum = raw_input ("What is the minimum number?: ")
-maximum = raw_input ("What is the maximum number?: ")
-thinking = raw_input ("I'm thinking of a number:")
+    minimum = raw_input ("What is the minimum number?: ")
+    maximum = raw_input ("What is the maximum number?: ")
+main()
+
+def output (minimum, maximum):
+    out= """
+I'm thinking of a number from {} to {}.
+
+""" .format (minimum, maximum)
+
+def main ():
+    youranswer= raw_input ("What do you think it is?:" )
+main()
+ 
+def output (target, guess, result):
+    out= """
+
+
+The target was {}.
+Your guess was {}.
+That's under by {}-{}={}
+
+""" . format (target, guess, result)
+    return out
+
