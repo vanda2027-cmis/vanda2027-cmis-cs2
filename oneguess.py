2dc2dae1a286166046bc9d67490d3ee6cec43582
cmis <cmis@cmis-hs08.(none)>
2016-03-16 11:55:17 +0700

    made changes

--- oneguess.py
+++ oneguess.py
@@ -1,28 +1,44 @@
 
-
 def main ():
     minimum = raw_input ("What is the minimum number?: ")
     maximum = raw_input ("What is the maximum number?: ")
+   
+    if target> str(abs(guess)):
+        guess_1 = guessing_1(target, guess, result)
+    print guess_1
+    elif target < str(abs(guess)):
+        guess_2 = guessing_2(target, guess, result)
+    print guess_2
+    elif target == str(abs(guess)):
+        guess_3 = guessing_3(target, guess, result)
+    print guess_3
 main()
 
-def output (minimum, maximum):
-    out= """
-I'm thinking of a number from {} to {}.
 
-""" .format (minimum, maximum)
-
-def main ():
-    youranswer= raw_input ("What do you think it is?:" )
-main()
- 
-def output (target, guess, result):
-    out= """
-
+def guessing_1 (target, guess , result):
+    guess_1= """
 
 The target was {}.
 Your guess was {}.
-That's under by {}-{}={}
+That's under by {}.
 
 """ . format (target, guess, result)
-    return out
+
+def guessing_2 (target, guess , result):
+    guess_2= """
+
+The target was {}.
+Your guess was {}.
+That's under by {}.
+
+""" . format (target, guess, result)
+def guessing_3 (target, guess , result):
+    guess_3= """
+
+The target was {}.
+Your guess was {}.
+That's under by {}.
+
+""" . format (target, guess, result)
+
 
