128d66c964f47ed98905152f1ffb25a7f6fe05ff
cmis <cmis@cmis-hs20.(none)>
2016-03-22 09:51:55 +0700

    done

--- oneguess.py
+++ oneguess.py
@@ -1,50 +1,50 @@
-
-def main ():
-    import random
-    minimum = raw_input ("What is the minimum number?: ")
-    maximum = raw_input ("What is the maximum number?: ")
-    random = random.randint (minimum, maximum)
-    print "I'm thinking of a number between", str(minimum), "and", str(maximum)
-    guess = raw_input ("What do you think it is?:")
-    difference = abs(guessnumber - random)
-
-   
-    if target> str(abs(guess)):
-        guess_1 = guessing_1(target, guess, result)
-    print guess_1
-    elif target < str(abs(guess)):
-        guess_2 = guessing_2(target, guess, result)
-    print guess_2
-    elif target == str(abs(guess)):
-        guess_3 = guessing_3(target, guess, result)
-    print guess_3
-main()
-
-
-def guessing_1 (target, guess , result):
+import random
+def guessing_1 (random1, guess , result):
     guess_1= """
 
 The target was {}.
 Your guess was {}.
 That's under by {}.
 
-""" . format (target, guess, result)
+""" . format (random1, guess, result)
+    return guess_1
 
-def guessing_2 (target, guess , result):
+def guessing_2 (random1, guess , result):
     guess_2= """
 
 The target was {}.
 Your guess was {}.
-That's under by {}.
+That's over by {}.
 
-""" . format (target, guess, result)
-def guessing_3 (target, guess , result):
+""" . format (random1, guess, result)
+    return guess_2
+def guessing_3 (random1, guess):
     guess_3= """
 
 The target was {}.
 Your guess was {}.
-That's under by {}.
+That's correct!.
 
-""" . format (target, guess, result)
+""" . format (random1, guess)
+    return guess_3
+def main ():
+    minimum = int(raw_input ("What is the minimum number?: "))
+    maximum = int (raw_input ("What is the maximum number?: "))
+    random1 = random.randint (minimum, maximum)
+    print "I'm thinking of a number between", str(minimum), "and", str(maximum)
+    guess = int(raw_input ("What do you think it is?:"))
+    result = abs(guess - random1)
+
+   
+    if random1>abs(guess):
+        guess_1 = guessing_1(random1, guess, result)
+        print guess_1
+    elif random1 <abs(guess):
+        guess_2 = guessing_2(random1, guess, result)
+        print guess_2
+    elif random1 ==abs(guess):
+        guess_3 = guessing_3(random1, guess)
+        print guess_3
+main()
 
 
