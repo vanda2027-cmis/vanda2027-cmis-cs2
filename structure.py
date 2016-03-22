28c07842b3bac552c22921178cb5a482fd7ba93f
cmis <cmis@cmis-hs20.(none)>
2016-03-22 09:55:06 +0700

    made changes

--- structure.py
+++ structure.py
@@ -1,22 +1,22 @@
 import math
 
 
-def mul(m,v):
-	x = m*v
-	return x
+def mul(mass,velocity):
+	momentum = mass*velocity
+	return momentum
 
-def output(name, x1, m, v, x):
+def output(name, momentum1, mass, velocity, momentum):
 	return """
 Hey {}!
 The momentum of the ball is {}
-To find the momentum of the ball you'll need: {} * {} = {}
-""".format(name, x1, m, v, x)
+To find the momentum of the ball you'll need to times the mass of the ball by its velocity: {} * {} = {}
+""".format(name, momentum1, mass, velocity, momentum)
 
 def main():
 	name= raw_input ("What is you name?:")
-	m = raw_input("Type in the mass of the ball: ")
-	v = raw_input("Type in the velocity of the ball: ")
-	x = mul(int(m), int(v))
-	print output(name, x, m, v, x)
+	mass = raw_input("Type in the mass of the ball: ")
+	velocity = raw_input("Type in the velocity of the ball: ")
+	momentum = mul(int(mass), int(velocity))
+	print output(name, momentum, mass, velocity, momentum)
 main()
 
