710e8b6781f746dbd0e25952607707aa0a544343
cmis <cmis@cmis-hs20.(none)>
2016-03-29 09:55:28 +0700

    not done

--- conditionals.py
+++ conditionals.py
@@ -2,23 +2,24 @@ import random
 # You are playing a game called "The Amagi Brilliant Park."
 # This game will be played in a cave.
 # You will be asked if you want to enter the cave or not.
-# If you choose to enter the cave, it will ask you which way you would like to go
-# While playing the game, weapons will be given to you depending which way you choose to go.
-# If you can find the exit of this game, you'll be rewarded.
+# If you choose to enter the cave, it will ask you which way you would like to go.
+# While playing the game,questions will be asked and you have to answer them correctly.
+# If you answer them correctly, you'll be rewarded with weapon and try to exit the cave.-
 
 def caveentrance (decision):
     if decision == "yes":
         print "There are two possible ways you can go after you enter the cave."
-    Playerdecision_2 = raw_input("Do you want to enter the left tunnle or the right tunnle?")
-        
+        Playerdecision_2 = raw_input("Do you want to enter the left tunnle or the right tunnle?")
+        leftorright (Playerdecision_2)
     elif decision == "no":
         print "Good Bye."
    
-
-     elif decision_2 == "left"
-        print "The weapon given to you is a sword. Use them to fight with the monster that's going to appear in this pathway! 
-    elif decision_2 == "Right"
+def leftorright (decision_2):
+    if decision_2 == "left":
+        print "The weapon given to you is a sword. Use them to fight with the monster that's going to appear in this pathway!"
+    elif decision_2 == "right":
         print "You are lucky! No monsters!"
+    
     
     
 
@@ -26,8 +27,11 @@ def caveentrance (decision):
 
 def main ():
     Playerdecision = raw_input("Do you want to enter to cave? ")
-    
     caveentrance (Playerdecision)
+    
+    
+    
+    
 
 
     
