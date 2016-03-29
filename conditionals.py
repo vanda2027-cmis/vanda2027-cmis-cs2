e9eefacb175cd26f31e4deb21762bc17a9edde13
vanda2027-cmis <vanda2027@cmis.ac.th>
2016-03-29 10:15:59 +0700

    Update conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -1,10 +1,12 @@
 import random
 # You are playing a game called "The Amagi Brilliant Park."
-# This game will be played in a cave.
+# This game will be played in a cave and you'll have an amount of energy in your body.
 # You will be asked if you want to enter the cave or not.
 # If you choose to enter the cave, it will ask you which way you would like to go.
-# While playing the game,questions will be asked and you have to answer them correctly.
-# If you answer them correctly, you'll be rewarded with weapon and try to exit the cave.-
+# While playing the game,questions will be asked between you and the monsters. 
+# If you answer them correctly, you'll be rewarded with weapon to kill the monsters or something that gives you energy.
+# If you answer them incorrectly, you'll loose parts of your energy.
+# Try to survive!
 
 def caveentrance (decision):
     if decision == "yes":
@@ -16,7 +18,7 @@ def caveentrance (decision):
    
 def leftorright (decision_2):
     if decision_2 == "left":
-        print "The weapon given to you is a sword. Use them to fight with the monster that's going to appear in this pathway!"
+        print "The monster is waiting for you inside the cave. You have no weapons and you cannot choose to quit. This means that you'll loose parts of their energy!"
     elif decision_2 == "right":
         print "You are lucky! No monsters!"
     
@@ -28,7 +30,7 @@ def leftorright (decision_2):
 def main ():
     Playerdecision = raw_input("Do you want to enter to cave? ")
     caveentrance (Playerdecision)
-    
+    Seconddecision = raw_input ("There are two tresure boxes placed infront of you, would you like to open the left one or the right one?")
     
     
     
