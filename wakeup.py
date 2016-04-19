5871f1abeb199b016df2764251529acaba5b4656
cmis <cmis@cmis-hs20.(none)>
2016-04-19 11:17:22 +0700

    works

--- wakeup.py
+++ wakeup.py
@@ -33,8 +33,24 @@ def average():
     else:
         print str(n4) + " is out of range."
     return total / amoutofnumbers
+def evenorodd(num):
+    if num/2.0 == num/2:
+        return "even"
+    else:
+        return "odd"
+def output(fltavg, intavg, numberis):
+    out = """The average is {}
+The integer part of the average is {}.
+The integer part is {}.""".format(fltavg, intavg, numberis)
+    return out
 def main():
     print "\nThis program will ask you for 5 integer or float values.\nIt will calculate the average of all values from 0 inclusive to 10 exclusive.\nIt will print out whether the resulting average is even or odd.\n"
+    #Input    
     fltavg = average()
+    #Processing    
     intavg = int(fltavg)
+    numberis = evenorodd(intavg)
+    #Output    
+    res = output(fltavg, intavg, numberis)
+    print res
 main()
