c4035f5d9d99d8e640075f0f69ba549ad6723c55
cmis <cmis@cmis-hs21.(none)>
2016-04-20 14:35:23 +0700

    not done

---
+++ recursive.py
@@ -0,0 +1,20 @@
+def countup(start, stop):
+    if start > stop:
+        print "blastoff!"
+    else:
+        print start
+        countup(start+1, stop)
+
+def main():
+    countup(1,123)
+main()
+
+def countup(n):
+    if n >= 0
+        print "blastoff!"
+    else:
+        print n
+        countup (n+1)
+
+
+
