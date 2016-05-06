ec86dfa1a013b24a02d1a2cfc971e147d2717981
cmis <cmis@cmis-hs20.(none)>
2016-05-06 11:55:18 +0700

    not done

--- guessgame.py
+++ guessgame.py
@@ -1,12 +1,28 @@
 import random
-def game(guess, radom)
-    random = random.randint(1,100)
-    guess = int(raw_input("Guess a number: "))
+def rounds(numrounds, correct):
+    if numrounds == 0:
+        return "You got " + str(correct) + "rounds correct."
+    else:
+        return "Starting round: " + str(numrounds)
     
+def game(guess, tries):
+    random = (random.randint(1,100))
+    put = int(raw_input("Guess a number: "))
     if random == guess:
         print "That's correct!"
-    elif random > guess
+        return game(guess, tries - 1)
+    elif random > guess:
         print "That's too high"
-    elif random < guess
+        return game(guess, tries - 1)
+    elif random < guess:
         print "That's too low"
+        return game(guess, tries - 1)
+
+def main():
+    res = rounds(3, 0)
+    print res
+    game(guess, tries)
+    
+    
+main()
 
